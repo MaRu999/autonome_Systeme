259cb5f2e8ce704a4aa1d78608a8e76caf5e780b
Matthias Rupp <mru3383@students.fhv.at>
2021-04-08 19:11:18 +0200

    Added second variant to triang forward (incorrect y for some reason)

--- semester2/merz/triang_forward_exercise.m
+++ semester2/merz/triang_forward_exercise.m
@@ -2,5 +2,6 @@ a = [0;0];
 b = [8;4];
 rpa = 180;
 rpb = 90;
-pos = triangulation_forwardscut(a, b, deg2rad(rpa), deg2rad(rpb));
-disp("Robot position: "), disp(pos);
+[pos1, pos2] = triangulation_forwardscut(a, b, deg2rad(rpa), deg2rad(rpb));
+disp("Robot position (first variant): "), disp(pos1);
+disp("Robot position (second variant): "), disp(pos2);
