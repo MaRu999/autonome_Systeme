f2986a310f42cc9d32d413658fab3ba4d7f408bb
Matthias Rupp <mru3383@students.fhv.at>
2021-05-19 23:44:38 +0200

    some improvements, attracting force works

--- semester2/merz/potential_field/force_at_point.m
+++ semester2/merz/potential_field/force_at_point.m
@@ -1,6 +1,9 @@
 function force = force_at_point(pos, k_att, k_rep, roh_min, obstacle_mids, obstacle_radii, goal_pos)
     force = attracting_force(k_att, pos, goal_pos);
+    pos = pos + force;
     obs_count = max(size(obstacle_mids));
     for i=1:obs_count
-        force = force + repelling_force(pos, obstacle_mids(:,i), obstacle_radii(i), roh_min, k_rep);
+        r_f = repelling_force(pos, obstacle_mids(:,i), obstacle_radii(i), roh_min, k_rep);
+        pos = pos + r_f;
+        force = force + r_f;
     end
--- semester2/merz/potential_field/homework_potential_field.m
+++ semester2/merz/potential_field/homework_potential_field.m
@@ -6,11 +6,11 @@ k_rep = 0.05;
 octave_offset = 0;
 field_num = 200;
 scaling_factor = 20;
-roh_min = 1 * 20;
+roh_min = 1 * scaling_factor;
 middle_obs1 = ([6;4] + octave_offset) * scaling_factor;
 radius_obs1 = 1 * scaling_factor;
 midlle_obs2 = ([7;8] + octave_offset) * scaling_factor;
-radius_obs2 = 0.5 * 20;
+radius_obs2 = 0.5 * scaling_factor;
 middle_obs3 = ([3; 2.2] + octave_offset) * scaling_factor;
 radius_obs3 = 0.5 * scaling_factor;
 obstacle_mids = [middle_obs1, midlle_obs2, middle_obs3];
@@ -23,39 +23,42 @@ for i=1:field_num
         u_atts(i,j) = u_att(k_att, goal_p, [i;j]);
     end
 end
+% scaling_factor = 1;
 figure();
 mesh((1:field_num)/scaling_factor, (1:field_num)/scaling_factor, u_atts/scaling_factor);
 xlabel("x");
 ylabel("y");
 zlabel("z");
 axis("ij");
-
+% scaling_factor = 20;
 u_reps = zeros(field_num,field_num);
 for j=1:field_num
     for i=1:field_num
-        u_reps(i,j) = 0;
         for k=1:max(size(obstacle_mids))
-            u_reps(i,j) = u_reps(i,j) + u_rep(k_rep, roh_min, [i;j], obstacle_mids(:,k), obstacle_radii(k));
+            u_reps(i,j) = u_reps(i,j) + u_rep(k_rep, roh_min, [i;j], obstacle_mids(:,k), obstacle_radii(k), scaling_factor);
         end
     end
 end
+% scaling_factor = 1;
+up_lim = 0.25;
 figure();
 mesh((1:field_num)/scaling_factor, (1:field_num)/scaling_factor, u_reps/scaling_factor);
 xlabel("x");
 ylabel("y");
 zlabel("z");
 hold on;
-vec_x = ([6, 6] + octave_offset);
-vec_y = ([4, 4] + octave_offset);
-vec_z = [0, 0.25];
+scaling_factor = 1;
+vec_x = ([6, 6] + octave_offset) * scaling_factor;
+vec_y = ([4, 4] + octave_offset) * scaling_factor;
+vec_z = [0, up_lim];
 plot3(vec_x, vec_y, vec_z);
-vec_x = ([7, 7] + octave_offset);
-vec_y = ([8, 8] + octave_offset);
-vec_z = [0, 0.25];
+vec_x = ([7, 7] + octave_offset) * scaling_factor;
+vec_y = ([8, 8] + octave_offset) * scaling_factor;
+vec_z = [0, up_lim];
 plot3(vec_x, vec_y, vec_z);
-vec_x = ([3, 3] + octave_offset);
-vec_y = ([2.2, 2.2] + octave_offset);
-vec_z = [0, 0.25];
+vec_x = ([3, 3] + octave_offset) * scaling_factor;
+vec_y = ([2.2, 2.2] + octave_offset) * scaling_factor;
+vec_z = [0, up_lim];
 plot3(vec_x, vec_y, vec_z);
 hold off;
 axis("ij");
